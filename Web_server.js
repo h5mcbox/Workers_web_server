3546c77eaf7bb64a0a260967394822331596a688
h5mcbox <33593244+h5mcbox@users.noreply.github.com>
2020-07-22 12:50:49 +0800

    Create Web_server.js

---
+++ Web_server.js
@@ -0,0 +1,28 @@
+/*
+ * Worker web server
+ * https://github.com/h5mcbox/Worker_web_server
+ * 2020-07-22
+*/
+var route={}
+route["/"]=function(){
+  return ["A web server for cloudflare workers.",{status:200,headers:{}}]
+}
+route["/text"]="A example text page."
+route["/404"]=function(){
+  return ["404 Not found.",{status:404,headers:{}}]
+}
+/****************core****************/
+addEventListener('fetch', e => {
+  e.respondWith(handleRequest(e.request))
+})
+async function handleRequest(request){
+  return res(route[(new URL(request.url)).pathname])
+}
+async function res(body=route["/404"],status=200,headers={}){
+  if(typeof body==="function"){
+    var func=body()
+    return new Response(func[0],func[1])
+  }else{
+    return new Response(body, {status,headers})
+  }
+}
