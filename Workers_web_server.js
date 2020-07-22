b1db248951c0fb48545244d30386c828c98b6ddc
h5mcbox <33593244+h5mcbox@users.noreply.github.com>
2020-07-22 17:44:40 +0800

    Update and rename Web_server.js to Workers_web_server.js

--- Workers_web_server.js
+++ Workers_web_server.js
@@ -4,8 +4,8 @@
  * 2020-07-22
 */
 var route={}
-route["/"]=function(){
-  return ["A web server for cloudflare workers.",{status:200,headers:{}}]
+route["/"]=function(url){
+  return ["A web server for cloudflare workers.\nlocation.href="+url.href,{status:200,headers:{}}]
 }
 route["/text"]="A example text page."
 route["/404"]=function(){
@@ -16,19 +16,20 @@ addEventListener('fetch', e => {
   e.respondWith(handleRequest(e.request))
 })
 async function handleRequest(request){
-  return res(route[(new URL(request.url)).pathname])
+  return res(request.url,route[(new URL(request.url)).pathname])
 }
-async function res(body=route["/404"],status=200,headers={}){
+async function res(url,body=route["/404"],status=200,headers={}){
+  var url=new URL(url)
   try{
     if(typeof body==="function"){
-      var func=body()
+      var func=body(url)
       return new Response(func[0],func[1])
     }else{
       return new Response(body, {status,headers})
     }
   }catch(e){
     if(route["/500"]){
-      var func=route["/500"](e.name,e.message)
+      var func=route["/500"](url,e.name,e.message)
       return new Response(func[0],func[1])
     }
     return new Response("Web server gots some error.\n"+e.name+" "+e.message, {status:500,headers:{}})
