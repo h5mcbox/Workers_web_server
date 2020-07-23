378d870a677df93b567e296fd3c47837dad757b9
h5mcbox <33593244+h5mcbox@users.noreply.github.com>
2020-07-23 23:07:24 +0800

    Update Workers_web_server.js

--- Workers_web_server.js
+++ Workers_web_server.js
@@ -4,20 +4,33 @@
  * H503mc & h5mcbox
  * 2020-07-22
 */
+/* Documention
+ * 1:route["/pages"]=function(url){return [text,option]}  function-based pages
+ *   text:return text
+ *   option:{status:...,headers:...}
+ * 2:route["/textpages"]="string"
+ */
+ 
 var route={}
-route["/"]=function(url){
+route["/"]=function(url){                                                                        //Web root(/)
   var headers={}
   headers["content-type"]="text/html"
   return ["<h1>A web server for cloudflare workers.\n<br>location.href="+url.href+"</h1>",{status:200,headers}]
 }
-route["/text"]="A example text page."
+route["/text"]="A example text page."                                                            //a text-based page(/text)
 route["/404"]=function(){
   return ["404 Not found.",{status:404,headers:{}}]
-}
+}                                                                                                //404 page template(/404)
+route["/500"]=function(url){
+  if(url.pathname="/500"){
+    return route["/404"](url)
+  }                                                                                              //hide 500 page
+  return ["Web server gots some error.\n"+e.name+" "+e.message,{status:500,headers:{}}]
+}                                                                                                //custom 500 error page(/500)
 route["/error"]=function(){
   throw new Error("Throw an error.")
-}
-/****************Core****************/
+}                                                                                                //Throw an error then show 500 page
+/****************Core start****************/
 addEventListener('fetch', e => {
   e.respondWith(handleRequest(e.request))
 })
@@ -41,3 +54,4 @@ async function res(url,body=route["/404"],status=200,headers={}){
     return new Response("Web server gots some error.\n"+e.name+" "+e.message, {status:500,headers:{}})
   }
 }
+/****************Core end****************/
