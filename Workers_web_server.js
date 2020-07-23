e0cba07f968f4a55080bb69999866d557c2607c2
h5mcbox <33593244+h5mcbox@users.noreply.github.com>
2020-07-23 23:09:07 +0800

    Update Workers_web_server.js

--- Workers_web_server.js
+++ Workers_web_server.js
@@ -6,6 +6,7 @@
 */
 /* Documention
  * 1:route["/pages"]=function(url){return [text,option]}  function-based pages
+ *   url:a URL() Object
  *   text:return text
  *   option:{status:...,headers:...}
  * 2:route["/textpages"]="string"
